6625e4bb8274d0c5da570c0fb57b3b54c30667e5
Marcelo Bohn <marcelo.bohn@gmail.com>
2016-02-05 21:30:36 -0200

    Create Model Classroom

---
+++ spec/support/factories/classrooms.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :classroom do
+    student nil
+    course nil
+    entry_at "2016-02-05 21:19:06"
+  end
+end
