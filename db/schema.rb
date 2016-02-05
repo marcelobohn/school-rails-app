67be7a50a0e3aea989521be94ab40eb01e241d4e
Marcelo Bohn <marcelo.bohn@gmail.com>
2016-02-05 20:45:26 -0200

    Create scaffold Student

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160205222134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.string   "register_number"
+    t.integer  "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
