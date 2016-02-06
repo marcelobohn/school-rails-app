20ae7e6f3e47e4158afb562a998c0ec2a74524d6
Marcelo Bohn <marcelo.bohn@gmail.com>
2016-02-06 00:22:56 -0200

    Insert info in wellcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    @last_enroolment = Classroom.last
+    @total_students = Student.all.count
+    @total_courses = Course.all.count    
   end
 end
