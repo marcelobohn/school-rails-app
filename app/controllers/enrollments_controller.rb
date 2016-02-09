1d6c81e12a11ae7eeb43a28b421bd3477b5072ff
Marcelo Bohn <marcelo.bohn@gmail.com>
2016-02-09 18:49:11 -0200

    new test to Enrollments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -7,6 +7,6 @@ class EnrollmentsController < ApplicationController
   def create
     # render :text => params[:entry_at]
     Classroom.create student_id: params[:student_id], course_id: params[:course_id], entry_at: params[:entry_at]
-    redirect_to root_path 
+    redirect_to root_path
   end
 end
--- spec/controllers/enrollments_controller_spec.rb
+++ spec/controllers/enrollments_controller_spec.rb
@@ -2,10 +2,34 @@ require 'rails_helper'
 
 RSpec.describe EnrollmentsController, :type => :controller do
 
+  let(:valid_attributes_course) {
+    {name: 'React', description: 'Create web components', status: 1}
+  }
+
+  let(:valid_attributes_student) {
+    {name: 'marcelo', register_number: 'B01', status: 1}
+  }
+
+  let(:valid_attributes) {
+    {student: valid_attributes_student, course: valid_attributes_course, entry_at: "2016-02-05 21:19:06"}
+  }
+
+  let(:valid_session) { {} }  
+
   describe "GET new" do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST create" do
+    describe "with valid params" do
+      it "creates a new Course" do
+        expect {
+          post :create, {:classroom => valid_attributes}, valid_session
+        }.to change(Classroom, :count).by(1)
+      end
     end
   end
 
